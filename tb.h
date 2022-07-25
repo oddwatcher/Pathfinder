a44ff91373a05ce437297c394a4c4597d142f03b
oddwatcher <science-35@outlook.com>
2022-07-25 08:03:35 +0800

    swift

--- tb.h
+++ tb.h
@@ -15,66 +15,49 @@ typedef struct
     edge *head;
 } netnode;
 
-edge *addedge(char data, edge *n)
+int initnetnode(char data, netnode *nethead) //generates the node list without edges with a given nethead which indicates the beginnning of the net
 {
-    edge *newnode = (edge *)malloc(sizeof(edge));
-    newnode->data = data;
-    newnode->n = n;
-    return newnode;
-}
-
-netnode *addnetnode(char data, netnode *l, netnode *r)
-{
-    netnode *newnode = (netnode *)malloc(sizeof(netnode));
-    newnode->head = addedge(data,NULL);
-    newnode->name = data;
-    newnode->l = l;
-    newnode->r = r;
-    return newnode;
-}
-
-edge *appendedge(edge *R, char din)
-{
-    edge *temp = addedge(din,NULL);
-    R->n = temp;
-    return temp;
-}
-
-netnode *appendnetnode(netnode *R, char din)
-{
-    netnode *temp = addnetnode(din, R, R->r);
-    R->l = temp;
-    if (R->r != NULL)
+    int p;
+    netnode *newnode;
+    printf("number of nodes:");
+    scanf("%d", &p);
+    for (int i =p ; i > 0; i--)
     {
-        (R->r)->l = temp;
-    }
-    return temp;
-}
-
-int findedge(char t, edge *head)
-{
-    while (head->n != NULL)
-    {
-        head = head->n;
-        if (head->data == t)
-        {
-            return 1;
+        newnode = (netnode *)malloc(sizeof(netnode));
+        scanf("%c\n",newnode->name);
+        newnode->l = nethead;
+        newnode->r = nethead->r;
+        if(nethead->r!=NULL){
+            (nethead->r)->l =newnode;
         }
+        nethead->r = newnode;
     }
-    return 0;
+    return p;//number of nodes
+
 }
 
-netnode * findnetnode(char t, netnode *head)
+netnode *findnet(char name, netnode *head)
 {
-    while (head->r != NULL)
+    for (netnode *p = head; p != NULL; p = p->r)
     {
-        head = head->r;
-        if (head->name == t)
+        if (p->name == name)
         {
-            return head;
+            return p;
         }
     }
     return NULL;
+}
+
+edge *addedge(char data, netnode *nethead)
+{
+    char e;
+    scanf("%c", &e);
+    netnode *node = findnet(e, nethead);
+    edge *newnode = (edge *)malloc(sizeof(edge));
+    newnode->data = data;
+    newnode->n = (node->head)->n;
+    node->head = newnode;
+    return newnode;
 }
 
 void output(edge *p)
@@ -84,56 +67,4 @@ void output(edge *p)
         printf("%d,%p,%p,%p\n", p->data, p->n);
         p = p->n;
     } while (p != NULL);
-}
-
-netnode *netinit()
-{
-    netnode *N = addnetnode('0', NULL, NULL);
-    char C = '0';
-    int i = 0;
-    printf("Enter all nodes,end with !\n");
-    while (C != '!')
-    {
-        scanf("%c", &C);
-        appendnetnode(N, C);
-        i++;
-    }
-    printf("Total : %d", i);
-    return N;
-}
-
-int initEDGE(netnode *N){
-    char C = '1';
-    netnode *temp = N;
-    edge *p = NULL;
-    netnode *f = NULL;
-    while (temp->r!=NULL)
-    {
-        temp = temp->r;
-        printf("edge:%c,connected edge :",temp->name);
-        p = temp->head;
-        while (p->n!=NULL)
-        {
-            printf("%c,",p->data);
-            p = p->n;
-        }
-        printf("\n input new edge connected to %c end with 0",temp->name);
-        while (C != '0')
-        {
-            scanf("%c",&C);
-            netnode *f = findnetnode(C,N);
-            if(f!=NULL){
-                 appendedge(temp->head,C);
-                 appendedge(f->head,temp->name);
-            }else{
-                printf("node not decleared\n");
-            }
-           
-        }
-        
-
-        
-        
-    }
-    
 }
